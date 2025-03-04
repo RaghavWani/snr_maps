a8acce63675902e900b1d294b30fbf601fdda2b0
raghavwani <raghav.wani@gmail.com>
2025-03-05 00:15:44 +0530

    Combined SNR Plotting with PFD file processing code

---
+++ process_pfd_plot.py
@@ -0,0 +1,234 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import argparse
+import logging
+from matplotlib.ticker import FormatStrFormatter
+from astropy import units as u
+from astropy.coordinates import Angle
+# import h5py
+import os
+from pathlib import Path
+import warnings
+import yaml
+
+warnings.filterwarnings('ignore')
+
+logging.basicConfig(level="INFO", datefmt="[%X]", format="%(message)s")
+log = logging.getLogger("snr_plot")
+
+# Function to load YAML config file
+def load_config(config_path="config.yaml"):
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"Config file '{config_path}' not found. Please create one.")
+    
+    with open(config_path, "r") as file:
+        try:
+            config = yaml.safe_load(file)
+        except yaml.YAMLError as e:
+            raise ValueError(f"Error parsing the YAML file: {e}")
+    
+    return config
+
+# Function to get ra dec from ahdr files 
+# [This function WILL NOT BE USED Once Ra Dec in filterbank issue is fixed]
+def ra_dec_from_ahdr(directory_path, beam_per_host):
+    """
+    Extracts RA, DEC, BM-Idx, and BM-SubIdx values from ahdr files in a directory.
+    Input: Directory containing ahdr files.
+    Returns: A DataFrame containing the extracted data: RA, DEC, BM-Idx, BM-SubIdx.
+    """
+    data_columns = ["RA", "DEC", "BM-Idx", "BM-SubIdx"]
+    ahdr_data = pd.DataFrame(columns=data_columns)
+
+    # List all .ahdr files in the directory
+    ahdr_files = [os.path.join(directory_path, file) for file in os.listdir(directory_path) if file.endswith(".ahdr")]
+    if not ahdr_files:
+        print(f"No header files found in directory: {directory_path}")
+        return None
+    
+    for file in ahdr_files:
+        if os.path.exists(file):
+            with open(file, "r") as infile:
+                lines = infile.readlines()
+            
+                selected_lines = lines[28:28+beam_per_host]
+                
+                temp_df = pd.DataFrame([line.strip().split() for line in selected_lines], columns=data_columns)
+                ahdr_data = pd.concat([ahdr_data, temp_df], ignore_index=True)
+        else:
+            print(f"File {file} not found!")
+
+    ahdr_data = ahdr_data.apply(lambda col: pd.to_numeric(col, errors='coerce'))
+    return ahdr_data
+
+# Snr data from pfd files
+def extract_snr(pfd_dir,ahdr_data,nbeams, pfd_code):
+    '''
+    Extracts beam index and snr from folding outputs *.pfd.bestprof files 
+    Returns a new dataframe RA, DEC, BM-Idx, BM-SubIdx, SNR
+    '''
+    data_columns = ["BM-Idx", "SNR"]
+    pfd_data = pd.DataFrame(columns=data_columns)
+
+    for i in range(nbeams):
+        filename = f"L{i}{pfd_code}.pfd.bestprof"
+        file_path = os.path.join(pfd_dir, filename)
+        
+        if os.path.isfile(file_path) and filename.endswith('.pfd.bestprof'):
+            with open(file_path, "r") as f:
+                lines = f.readlines()
+            
+                selected_line = lines[13]
+                
+                temp_df = pd.DataFrame({
+                    'BM-Idx': [i],
+                    'SNR': [np.float64(selected_line.split("(")[2].split("~")[1].split(" ")[0])]                      
+                })
+                pfd_data = pd.concat([pfd_data, temp_df], ignore_index=True)
+        else:
+            raise ValueError(f"No pfd file found for Beam {i}!")
+        
+    merged_df = pd.merge(ahdr_data, pfd_data,on='BM-Idx', how='inner')
+    return merged_df
+
+# SNR plotting
+def fold_snr_plot(merged_df, src_name, band, output_dir):
+    '''
+    Plots SNR scatter plot for merged dataframe. Saves the data and folded snr map to output directory.
+    '''
+    output_path = os.path.join(output_dir, f"SNRMap_{src_name}_B{band}.csv")
+    merged_df.to_csv(output_path, index=False)
+    
+    # fold_sm = pd.read_csv(merged_df,delimiter=',')
+    merged_df.sort_values(by=['BM-Idx'],ascending=True,inplace=True)
+    # merged_df.drop(columns=['I'],inplace=True)
+    merged_df.reset_index(drop=True, inplace=True)
+
+    #Highest SNR RA DEC used as source coordinates
+    src_ra = merged_df[merged_df['SNR'] == 1]['RA']
+    src_dec = merged_df[merged_df['SNR'] == 1]['DEC']
+
+    print('Max SNR:',merged_df['SNR'].max(),', Min SNR:',merged_df['SNR'].min())
+    #Shift and renormalization
+    merged_df['SNR'] -= min(merged_df['SNR']) #Note negative sign
+    merged_df['SNR'] /= max(merged_df['SNR'])
+    print('Renormalizing...')
+    print('Max SNR:',merged_df['SNR'].max(),', Min SNR:',merged_df['SNR'].min())
+
+    #SNR Map from data:
+    fig,ax = plt.subplots(figsize=(5,4))
+    scatter = ax.scatter(merged_df['RA'], merged_df['DEC'], c=merged_df['SNR'], s=50, cmap='viridis', edgecolors='black', alpha=1)
+    fig.colorbar(scatter, ax=ax, label='SNR')
+    ax.plot(src_ra, src_dec, 'o',markersize=2, label='Pulsar Coord', color='red')
+    ax.set_xlabel('Right Ascension (rad)')
+    ax.set_ylabel('Declination (rad)')
+    ax.set_title(f'Folded SNR Map: {src_name}, Band {band}')
+    plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%.4f'))
+    plt.legend()
+    output_path = os.path.join(output_dir, f"SNRMapFOLD_{src_name}_B{band}.png")
+    plt.savefig(output_path)
+    plt.show()
+
+    return merged_df
+
+def sim_snr_plot(sim_file, src_name, band, output_dir):
+    '''
+    Plots SNR scatter plot for simulation data. Saves the simulated snr map to output directory.
+    '''
+
+    # Simulation SNR map data:
+    sim_sm = pd.read_csv(sim_file, delim_whitespace=True, header=None) 
+    sim_sm.columns = ['RA', 'DEC', 'SNR', 'Beam-Idx']
+
+    print('Max:',sim_sm['SNR'].max(),', Min:',sim_sm['SNR'].min())
+
+    sim_sm['SNR'] -= min(sim_sm['SNR']) #Shift and renormalization
+    sim_sm['SNR'] /= max(sim_sm['SNR'])
+    print('Renormalizing...')
+    print('Max:',sim_sm['SNR'].max(),', Min:',sim_sm['SNR'].min()) #should be 1 and 0
+
+    #arcsec to radian 
+    sim_sm['RA'] = sim_sm['RA'].apply(lambda x: (x * u.arcsec).to(u.rad).value)
+    sim_sm['DEC'] = sim_sm['DEC'].apply(lambda x: (x * u.arcsec).to(u.rad).value)
+
+    #Linear transformation (ra dec shift): replace the RA and DEC values with the phase center ra dec values
+    # sim_sm['RA'] += pc_ra 
+    # sim_sm['DEC'] += pc_dec
+
+    fig,ax = plt.subplots(figsize=(5,4))
+    scatter = ax.scatter(sim_sm['RA'], sim_sm['DEC'], c=sim_sm['SNR'], s=50, cmap='viridis', edgecolors='black', alpha=1)
+    fig.colorbar(scatter, ax=ax, label='SNR')
+    # ax.plot(src_ra, src_dec, 'o',markersize=2, label='Pulsar Coord', color='red')
+    ax.set_xlabel('Right Ascension (rad)')
+    ax.set_ylabel('Declination (rad)')
+    ax.set_title(f'Simulation SNR Map: {src_name}, Band {band}')
+    # plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%.4f'))
+    plt.legend()
+
+    output_path = os.path.join(output_dir, f"SNRMap_{src_name}_B{band}.png")
+    plt.savefig(output_path)
+    plt.show()
+
+    return sim_sm
+
+def residual_plot(fold_sm, sim_sm, src_name, band):
+    '''
+    Plots residual scatter plot for simulation and folded data.
+    '''
+
+    # Residual calculation
+    residual = abs(fold_sm['SNR'] - sim_sm['SNR'])
+    #Residual Plot:
+    print(f"Max residual:\n{round(max(residual),2)}")
+    fig,ax = plt.subplots(figsize=(5,4))
+    scatter = ax.scatter(sim_sm['RA'], sim_sm['DEC'], c=residual, s=50, cmap='viridis', edgecolors='black', alpha=1)
+    fig.colorbar(scatter, ax=ax, label='SNR')
+    # ax.plot(src_ra, src_dec, 'o',markersize=2, label='Pulsar Coord', color='red')
+    ax.set_xlabel('Right Ascension (rad)')
+    ax.set_ylabel('Declination (rad)')
+    ax.set_title(f'SNR Residual Plot: {src_name}, Band {band}')
+    plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%.4f'))
+    plt.legend()
+    # plt.savefig(output_dir / f"SNRMapRES_{src_name}_B{band}.png")
+    plt.show()
+
+# Main function
+def main():
+    try:
+        config = load_config()
+        
+        # Details from config file
+        src_name = config.get("src_name")
+        pfd_code = config.get("pfd_code")
+        band = config.get("band")
+
+        header_dir_path = config.get("header_dir_path")
+        pfd_dir_path = config.get("pfd_dir_path")
+        sim_file_path = config.get("sim_file_path")
+        output_dir_path = config.get("output_dir_path")
+        bph = config.get("beam_per_host")
+        nbeams = config.get("nbeams")
+        log_level = config.get("log_level", "INFO")
+
+        log.info(f"Processing PFD files from {header_dir_path} with {nbeams} beams...")
+        print(f"Logging level set to: {log_level}")
+        
+        #Main script
+        ahdr_data = ra_dec_from_ahdr(header_dir_path,bph)
+        print('done ahdr data')
+        new_data = extract_snr(pfd_dir_path,ahdr_data,nbeams, pfd_code)
+        print('done new data')
+        fold_sm = fold_snr_plot(new_data, src_name, band, output_dir_path)
+        print('done fold sm')
+        sim_sm = sim_snr_plot(sim_file_path, src_name, band, output_dir_path)
+        print('done sim sm')
+        residual_plot(fold_sm, sim_sm, src_name, band)
+
+        print(f"SNR Map data and plot saved to {output_dir_path}")
+    
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main() 
